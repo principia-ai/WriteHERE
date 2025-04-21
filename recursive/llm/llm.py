5725df60a0f283a8e7e173996a4808f827fb0df3
Beastlyprime <452757387@qq.com>
2025-04-21 17:48:36 +0300

    fix typo

--- recursive/llm/llm.py
+++ recursive/llm/llm.py
@@ -108,7 +108,7 @@ class OpenAIApiProxy():
                     break  # Successful response, exit the loop
                 else:
                     # print(f"Received status code {response.status_code} at attempt={attempt + 1}. Retrying... {current_headers['x-api-key']=}")
-                    print(f"Received status code {response.status_code} at attempt={attempt + 1}. Retrying..., the reponse is {response.text}", flush=True)
+                    print(f"Received status code {response.status_code} at attempt={attempt + 1}. Retrying..., the response is {response.text}", flush=True)
                     
                     
             except requests.exceptions.RequestException as e:
@@ -335,7 +335,7 @@ class OpenAIApiProxy():
                         # just return None
                         return None
     
-                    print(f"Received status code {response.status_code} at attempt={attempt + 1}. Retrying..., the reponse is {response.text}", flush=True)  
+                    print(f"Received status code {response.status_code} at attempt={attempt + 1}. Retrying..., the response is {response.text}", flush=True)  
                     
             except requests.exceptions.RequestException as e:
                 print(f"Error making request (attempt {attempt + 1}): {e}", flush=True)
